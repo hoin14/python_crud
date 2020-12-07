52a1553ba058dbaf82c47de6668785ced185c939
hoin14 <zazahoin@gmail.com>
2020-12-07 23:52:33 +0900

    post

--- crudprojects/crudapp/views.py
+++ crudprojects/crudapp/views.py
@@ -67,23 +67,23 @@ def workinfo(requset):
     day = time.strftime('%d', time.localtime(time.time()))
     Date = year + '-' + month + '-'
     DateList = []
-    DayList = []
     for i in range(1, 31):
         DateList.append(Date+ str(i))
-        DayList.append(str(i))
-    return render(requset, 'workinfo.html', {'Date' : DateList, 'Day' : DayList})
+    return render(requset, 'workinfo.html', {'Date' : DateList})
 
 def workadd(requset):
     if requset.method == "POST":
-        sTime = requset.POST.get('startTime', '')
-        eTime = requset.POST.get('endTime', '')
+        sTime = requset.POST.get('sTime', '')
+        eTime = requset.POST.get('eTime', '')
         user = requset.POST.get('user', '')
-        sTimeArray = [];
-        sTimeArray = requset.getParameterValues("startTime");
+        # sTimeArray = [];
+        # sTimeArray = requset.getParameterValues("sTime");
+        param = json.loads(request.json)
+        sTime = param.get('sTime')
+        # print(sTimeArray)
 
-        print(sTimeArray)
-        print(eTime)
-        print(user)
+        print(sTime)
+
         # #csv파일 쓰기
         # f = open('workinfo/' + date + '_' + user + '.csv', 'w', encoding='utf-8', newline='')
         # wr = csv.writer(f)
@@ -103,6 +103,7 @@ def worksearch(requset):
         f = open('workinfo/' + sYear + sMonth + '_' + sName + '.csv', 'r', encoding='utf-8')
         rdr = csv.reader(f)
         workList = []
+        print(rdr)
         for line in rdr:
             workList.append(line)
         f.close()
